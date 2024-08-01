f4b20c848f1766f403b2d7660d8bb52a44aa8422
Yumi Yukimura <me.cafebabe@gmail.com>
2024-08-01 17:40:19 +0800

    virtio_arm64: Rename a variable to TARGET_PREBUILT_EMULATOR_KERNEL_USE
    Change-Id: I61d28836ca156c1e8e006df2eaac1b0918a562a6

--- device.mk
+++ device.mk
@@ -5,9 +5,9 @@
 #
 
 # Kernel (Prebuilt)
+TARGET_PREBUILT_EMULATOR_KERNEL_USE ?= 6.6
 TARGET_PREBUILT_KERNEL_ARCH := arm64
 TARGET_PREBUILT_KERNEL_MODULES_ARCH := arm64
-TARGET_PREBUILT_KERNEL_USE ?= 6.6
 
 # Inherit from common
 $(call inherit-product, device/virt/virtio-common/device-common.mk)
