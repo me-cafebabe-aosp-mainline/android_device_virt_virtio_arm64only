4045401756e36adf96538ec1cfb7f66745f0f02d
Yumi Yukimura <me.cafebabe@gmail.com>
2024-09-27 03:43:09 +0800

    virtio_arm64only: Add LMODroid product makefile
    Change-Id: Ia592ce31ef1e7e78927da39431f6c144697d2021

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -6,8 +6,10 @@
 
 PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/aosp_virtio_arm64only.mk \
-    $(LOCAL_DIR)/lineage_virtio_arm64only.mk
+    $(LOCAL_DIR)/lineage_virtio_arm64only.mk \
+    $(LOCAL_DIR)/lmodroid_virtio_arm64only.mk
 
 $(foreach build_type, user userdebug eng, \
     $(eval COMMON_LUNCH_CHOICES += aosp_virtio_arm64only-$(build_type)) \
-    $(eval COMMON_LUNCH_CHOICES += lineage_virtio_arm64only-$(build_type)))
+    $(eval COMMON_LUNCH_CHOICES += lineage_virtio_arm64only-$(build_type)) \
+    $(eval COMMON_LUNCH_CHOICES += lmodroid_virtio_arm64only-$(build_type)))
---
+++ lmodroid_virtio_arm64only.mk
@@ -0,0 +1,31 @@
+#
+# Copyright (C) 2024 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit_only.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base.mk)
+
+# Inherit some common LMODroid stuff.
+$(call inherit-product, vendor/lmodroid/config/common_full_tablet_wifionly.mk)
+
+# Inherit from device
+$(call inherit-product, device/virt/virtio_arm64only/device.mk)
+
+PRODUCT_NAME := lmodroid_virtio_arm64only
+PRODUCT_DEVICE := virtio_arm64only
+PRODUCT_BRAND := VirtIO
+PRODUCT_MANUFACTURER := VirtIO
+PRODUCT_MODEL := VirtIO arm64-only
+
+PRODUCT_VENDOR_PROPERTIES += \
+    ro.soc.manufacturer=$(PRODUCT_MANUFACTURER) \
+    ro.soc.model=$(PRODUCT_DEVICE)
+
+# Workaround build fingerprint too long
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRIVATE_BUILD_DESC="virtio_arm64only-eng 14 AP2A.240705.005 0 test-keys"
+
+BUILD_FINGERPRINT := virtio/virtio_arm64only/virtio_arm64only:14/AP2A.240705.005/0:eng/test-keys
