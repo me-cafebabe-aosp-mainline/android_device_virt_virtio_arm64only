e9905685a0b416aced21ed889ee22a6a11d9bd76
Yumi Yukimura <me.cafebabe@gmail.com>
2024-10-26 03:27:33 +0800

    virtio_arm64only: Remove build fingerprint override
    * Overriding no longer works in this way
    * The main thing is, it's no longer needed?
    
    Change-Id: I733720949b9272dd1b2c342b288bed0429c4ef48

--- aosp_virtio_arm64only.mk
+++ aosp_virtio_arm64only.mk
@@ -25,9 +25,3 @@ PRODUCT_MODEL := VirtIO arm64-only
 PRODUCT_VENDOR_PROPERTIES += \
     ro.soc.manufacturer=$(PRODUCT_MANUFACTURER) \
     ro.soc.model=$(PRODUCT_DEVICE)
-
-# Workaround build fingerprint too long
-PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRIVATE_BUILD_DESC="virtio_arm64only-eng 14 AP2A.240705.005 0 test-keys"
-
-BUILD_FINGERPRINT := virtio/virtio_arm64only/virtio_arm64only:14/AP2A.240705.005/0:eng/test-keys
