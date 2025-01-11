7e45eddf7252e2b3bf37997c956aee32ba25c693
Yumi Yukimura <me.cafebabe@gmail.com>
2025-01-12 01:04:17 +0800

    virtio_arm64only: Inherit everything from virtio_arm64
    * There's no difference at all, just except 2ND_ARCH
    * We already uses files from virtio_arm64
    
    Change-Id: I3500cdc0d2075776067e3a9b065d3a7eb4c854e8

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -4,41 +4,14 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-# Boot manager
-TARGET_BOOT_MANAGER ?= grub
-
-# Inherit from common
-include device/virt/virtio-common/BoardConfigCommon.mk
+# Inherit from virtio_arm64
+include device/virt/virtio_arm64/BoardConfig.mk
 
 USES_DEVICE_VIRT_VIRTIO_ARM64ONLY := true
 
 # Arch
-TARGET_ARCH := arm64
-TARGET_ARCH_VARIANT := armv8-a
-TARGET_CPU_ABI := arm64-v8a
-TARGET_CPU_ABI2 :=
-TARGET_CPU_VARIANT := generic
-
-# GRUB
-TARGET_GRUB_ARCH := arm64-efi
-TARGET_GRUB_HOST_PREBUILT_TAG := linux-arm64
-TARGET_GRUB_BOOT_EFI_PREBUILT := $(DEVICE_PATH)/../virtio_arm64/bootmgr/grub/prebuilt/boot/BOOTAA64.EFI
-TARGET_GRUB_INSTALL_EFI_PREBUILT := $(DEVICE_PATH)/../virtio_arm64/bootmgr/grub/prebuilt/install/BOOTAA64.EFI
-
-# Kernel
-BOARD_KERNEL_CMDLINE += \
-    console=ttyAMA0
-
-BOARD_KERNEL_IMAGE_NAME := Image
-TARGET_KERNEL_ARCH := arm64
-
-# Partitions
-ifeq ($(TARGET_LOGICAL_PARTITIONS_FILE_SYSTEM_TYPE),ext4)
-    ifneq ($(WITH_GMS),true)
-        BOARD_PRODUCTIMAGE_PARTITION_RESERVED_SIZE := 1073741824 # 1 GB
-    endif
-endif
-
-# SELinux
-BOARD_VENDOR_SEPOLICY_DIRS += \
-    $(DEVICE_PATH)/sepolicy/vendor
+TARGET_2ND_ARCH :=
+TARGET_2ND_ARCH_VARIANT :=
+TARGET_2ND_CPU_ABI :=
+TARGET_2ND_CPU_ABI2 :=
+TARGET_2ND_CPU_VARIANT :=
--- device.mk
+++ device.mk
@@ -4,16 +4,5 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-# Kernel (Prebuilt)
-TARGET_PREBUILT_EMULATOR_KERNEL_USE ?= 6.6
-TARGET_PREBUILT_KERNEL_ARCH := arm64
-TARGET_PREBUILT_KERNEL_MODULES_ARCH := arm64
-
-# Inherit from common
-$(call inherit-product, device/virt/virtio-common/device-common.mk)
-
-DEVICE_PATH := device/virt/virtio_arm64only
-
-# Soong namespaces
-PRODUCT_SOONG_NAMESPACES += \
-    $(DEVICE_PATH)
+# Inherit from virtio_arm64
+$(call inherit-product, device/virt/virtio_arm64/device.mk)
